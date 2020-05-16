27522643c4e9671a49ba0880be7244f33070e556
Matteo Paoli <m.paoli@mecoil.net>
2020-05-16 11:25:44 +0200

    move in util the log utility

--- matteopaoli.it.go
+++ matteopaoli.it.go
@@ -5,24 +5,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
-)
-
-var (
-	//Trace deeply info
-	Trace *log.Logger
-	//Info standard logs
-	Info *log.Logger
-	//Warning sameting to take care of
-	Warning *log.Logger
-	//Error somethings go wrong
-	Error *log.Logger
 )
 
 //ServerInfo info printed in / page
@@ -32,30 +20,6 @@ type ServerInfo struct {
 }
 
 var serverInfo = ServerInfo{Name: "Matteo", Surname: "Paoli"}
-
-//LogInit helpfull
-func LogInit(
-	traceHandle io.Writer,
-	infoHandle io.Writer,
-	warningHandle io.Writer,
-	errorHandle io.Writer) {
-
-	Trace = log.New(traceHandle,
-		"TRACE: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	Info = log.New(infoHandle,
-		"INFO: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	Warning = log.New(warningHandle,
-		"WARNING: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-
-	Error = log.New(errorHandle,
-		"ERROR: ",
-		log.Ldate|log.Ltime|log.Lshortfile)
-}
 
 //HomeHandler main path
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
